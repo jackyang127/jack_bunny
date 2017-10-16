375d6a2c06b50003c0072e8fbe7ef6eb31670e64
Jack Yang <jackyang@berkeley.edu>
2017-10-16 15:47:10 -0700

    additional features added. Help command now active

--- jack_bunny.py
+++ jack_bunny.py
@@ -1,25 +1,32 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, render_template, redirect
 import wikipedia
 
 app = Flask(__name__)
 
 class Commands(object):
 	def fb(arg=None):
-		"""searching on facebook"""
+		"""'fb [insert query]' searching on facebook. defaults on fb homepage"""
 		if arg:
 			return 'http://www.facebook.com/s.php?q={0}&init=q'.format(arg)
 		else:
 			return 'http://www.facebook.com'
 	
 	def g(arg=None):
-		"""searching google"""
+		"""'g [insert query]' searching google"""
 		if arg:
 			return 'http://www.google.com/search?q={0}'.format(arg)
 		else:
 			return 'http://www.google.com'
 
+	def gm(arg=None):
+		"""'gm [insert number from 0-n where n = number of gmail accounts - 1] opens up gmail. If no argument specificed, opens up the first account. Can open up alternative accounts with arguments"""
+		if arg:
+			return 'https://mail.google.com/mail/u/{0}/#inbox'.format(arg)
+		else:
+			return 'https://mail.google.com/mail/u/0/#inbox'
+
 	def w(arg=None):
-		"""searches wikipedia, defaults on english wikipedia page"""
+		"""'w [insert query]' searches wikipedia, defaults on english wikipedia page"""
 		try:
 			if arg:
 				# access the wikipedia api, first look for suggestions, then get
@@ -36,8 +43,15 @@ class Commands(object):
 		except:
 			return 'https://en.wikipedia.org/wiki/Main_Page'	
 
+	def cpp(arg=None):
+		"""'cpp [insert query]' searches for syntactical cpp terms on cppreference.com"""
+		if arg:
+			return 'http://en.cppreference.com/mwiki/index.php?title=Special%3ASearch&search={0}'.format(arg)
+		else:
+			return 'http://en.cppreference.com/w/'
+
 	def p(arg=None):
-		"""make a piazza search, kinda personalized just for jack lol"""
+		"""'p [insert class number]' make a piazza search, kinda personalized just for jack lol"""
 		# should update to use piazza api for more generalized case
 		mapping = {
 			'189': 'j2ji31rkkl2og',
@@ -50,12 +64,19 @@ class Commands(object):
 			return 'https://piazza.com'
 
 	def yt(arg=None):
-		"""make a youtube search"""
+		"""'yt [insert query]' make a youtube search. If not query is passed in, defaults to the youtube homepage"""
 		if arg:
 			return 'http://www.youtube.com/results?search_query={0}&search_type=&aq=-1&oq='.format(arg)
 		else:
 			return 'http://www.youtube.com'
 
+	def help(arg=None):
+		"""'help' returns a list of usable commands """
+		help_list = []
+		for values in Commands.__dict__.values():
+			if callable(values):
+				help_list.append(values.__doc__)
+		return help_list
 
 @app.route('/')
 def index():
@@ -66,7 +87,7 @@ def route(query):
 	#process the query
 	try:
 		tokenized_query = query.split(' ', 1)
-		search_command = tokenized_query[0]
+		search_command = tokenized_query[0].lower()
 		option_args = None
 		if len(tokenized_query) == 2:
 			option_args = tokenized_query[1]
@@ -76,9 +97,13 @@ def route(query):
 
 	try:
 		command = getattr(Commands, search_command)
+		if search_command == 'help':
+			print(command(None))
+			return render_template('help.html', command_list = command(None))
 		url = command(option_args)
 		return redirect(url)
-	except:
+	except Exception as e:
+		print(e)
 		# fallback option is to google search
 		return redirect(Commands.g(query))
 
